fa4d394121b422bbbae0cf7fb1fe77c75adcd651
Lakshya Sachin Bhardwaj <lakshyabh444@gmail.com>
2025-04-05 11:01:37 +0530

    Update 15-02-25-crud_test.java

--- 15-02-25-crud_test.java
+++ 15-02-25-crud_test.java
@@ -6,8 +6,8 @@ public class CRUD_Test {
   public static void main(String[] args){
     CRUD_Test objTest = new CRUD_Test();
 
-    objTest.create_data("1","Shubham","shubham@gmail.com");
-    objTest.create_data("2","Shubham","shubhamraj@gmail.com");
+    objTest.create_data("1","Lakshya","lakshya@gmail.com");
+    objTest.create_data("2","Lakshya","lakshyabhardwaj@gmail.com");
 
   }
 
