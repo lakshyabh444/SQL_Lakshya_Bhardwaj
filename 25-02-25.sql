7893037b709e6043833427cbd12fc812728ef8e8
Lakshya Sachin Bhardwaj <lakshyabh444@gmail.com>
2025-04-05 11:10:32 +0530

    Update 25-02-25.sql

--- 25-02-25.sql
+++ 25-02-25.sql
@@ -3,8 +3,8 @@ monday
 db.Students.find().pretty()
 {
   _id: ObjectId('67d9b946726d52f6470e5b76'),
-  name: 'Shubham Raj Sharma',
-  location: 'Bihar'
+  name: 'Lakshya Sachin Bhardwaj',
+  location: 'Indore'
 }
 -- db.Students.updateOne({name:'Shubham Raj Sharma'}, {$set:{'name':'Shubh'})
 -- SyntaxError: Unexpected token, expected "," (1:73)
@@ -31,7 +31,7 @@ db.Students.find().pretty()
 
 -- [0m[31m[1m>[22m[39m[90m 1 |[39m db[33m.[39m[33mStudents[39m[33m.[39mupdateOne({name[33m:[39m[32m'Shubham Raj Sharma'[39m}[33m,[39m{$set[33m:[39m{[32m'name'[39m[33m:[39m[32m'Shubh'[39m})
 --  [90m   |[39m                                                                         [31m[1m^[22m[39m[0m
-db.Students.updateOne({name:'Shubham Raj Sharma'},{$set:{'name':'Shubh'}})
+db.Students.updateOne({name:'Lakshya Sachin Bhardwaj'},{$set:{'name':'Aditya'}})
 {
   acknowledged: true,
   insertedId: null,
@@ -42,7 +42,7 @@ db.Students.updateOne({name:'Shubham Raj Sharma'},{$set:{'name':'Shubh'}})
 db.Students.find()
 {
   _id: ObjectId('67d9b946726d52f6470e5b76'),
-  name: 'Shubh',
+  name: 'Aditya',
   location: 'Bihar'
 }
 db.Students.insertOne({})
@@ -61,7 +61,7 @@ name: "Joy"});
 --  [90m 3 |[39m name[33m:[39m [32m"Joy"[39m})[33m;[39m[0m
 db.Students.insertOne({
 _id :1,
-name: "Joy"});
+name: "Harsh"});
 {
   acknowledged: true,
   insertedId: 1
@@ -69,7 +69,7 @@ name: "Joy"});
 db.Students.find()
 {
   _id: ObjectId('67d9b946726d52f6470e5b76'),
-  name: 'Shubh',
+  name: 'Aditya',
   location: 'Bihar'
 }
 {
@@ -77,7 +77,7 @@ db.Students.find()
 }
 {
   _id: 1,
-  name: 'Joy'
+  name: 'Harsh'
 }
 db.Students.insertOne({
 _id :null,
@@ -88,7 +88,7 @@ name: "Ujjwal"});
 }
 db.Students.insertOne({
 _id :null,
-name: "Shubham"});
+name: "Lakshya"});
 {
   acknowledged: true,
   insertedId: ObjectId('67d9c0c34528fa06b04ca586')
@@ -96,7 +96,7 @@ name: "Shubham"});
 db.Students.find()
 {
   _id: ObjectId('67d9b946726d52f6470e5b76'),
-  name: 'Shubh',
+  name: 'Aditya',
   location: 'Bihar'
 }
 {
@@ -104,7 +104,7 @@ db.Students.find()
 }
 {
   _id: 1,
-  name: 'Joy'
+  name: 'Harsh'
 }
 {
   _id: ObjectId('67d9c0bd4528fa06b04ca585'),
@@ -112,7 +112,7 @@ db.Students.find()
 }
 {
   _id: ObjectId('67d9c0c34528fa06b04ca586'),
-  name: 'Shubham'
+  name: 'Lakshya'
 }
 db.Students.insertOne([
   {title:"NoSQL Distilled",isbn:"0-4696-7030-4"},
