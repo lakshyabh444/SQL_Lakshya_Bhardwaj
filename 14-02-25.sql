d9c93154485b03b1a160dc726135effc411b4c45
Lakshya Sachin Bhardwaj <lakshyabh444@gmail.com>
2025-04-05 11:02:36 +0530

    Update 14-02-25.sql

--- 14-02-25.sql
+++ 14-02-25.sql
@@ -3,7 +3,7 @@ use friday;
 show tables from friday;
 
 create table student(id int, name varchar(20), location varchar(20));
-insert into student values (101, 'Shubham','Samastipur'),(102,'Joy','Kolkata');
+insert into student values (101, 'Lakshya','Indore'),(102,'Aditya','Kolkata');
 
 select *from student;
 
